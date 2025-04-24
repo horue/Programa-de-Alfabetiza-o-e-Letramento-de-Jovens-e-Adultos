2ef14720d48162eed1c6c0e989a971060e8b96c3
Jorge <jorgejapao00@gmail.com>
2025-04-24 19:11:28 -0300

    Styles moved to styles.js

--- App.js
+++ App.js
@@ -7,6 +7,8 @@ import {useState} from 'react';
 import { Card } from 'react-native-paper';
 
 // or any files within the Snack
+import { styles } from './styles.js';
+
 const d = new Date();
 let hour = d.getHours();
 
@@ -52,50 +54,3 @@ export default function App() {
     </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    backgroundColor: '#ecf0f1',
-    padding: 8,
-  },
-  paragraph: {
-    margin: 24,
-    fontSize: 12,
-    textAlign: 'center',
-  },
-  common_text: {
-    fontSize: 15,
-    marginBottom: 7,
-    color: "#000000",
-    textAlign: "left",
-  },
-  hyperlink:{
-    fontSize: 14,
-    margin: 7,
-    color: "#0000ff",
-    textAlign: "right",
-  },
-  hyperlink_clicked:{
-    fontSize: 14,
-    margin: 7,
-    color: "#0000cc",
-    textAlign: "right",
-    textDecorationLine: 'underline',
-  },
-  input: {
-    borderWidth: 1,
-    borderRadius: 6,
-    borderColor:"#000000",
-    height: 38,
-    backgroundColor: "#e0f0ff"
-  },
-    logo: {
-    height: 100,
-    width: 346,
-    resizeMode: "contain",
-    justifyContent: 'center',
-    position: 'center',
-  },
-});
