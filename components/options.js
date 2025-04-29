118702eecf3d929983e8a6f7d85560e5e2c31fda
Jorge <jorgejapao00@gmail.com>
2025-04-29 11:04:24 -0300

    options updated with dropdown attribute

--- components/options.js
+++ components/options.js
@@ -1,10 +1,10 @@
 // Dados
 const options = [
-  { id: 1, icon:"how-to-reg", option: "Realizar Chamada", screen:"realizar_chamada"},
-  { id: 2, icon:"note-add", option: "Adicionar Conteúdo"},
-  { id: 3, icon:"calendar-month", option: "Criar Turma"},
-  { id: 4, icon:"calendar-month", option: "Criar Aluno"},
-  { id: 5, icon:"person-add", option: "Adicionar Aluno"},
+  { id: 1, icon:"how-to-reg", option: "Realizar Chamada", dropdown:"realizar_chamada"},
+  { id: 2, icon:"note-add", option: "Adicionar Conteúdo", dropdown:"Adicionar Conteúdo"},
+  { id: 3, icon:"calendar-month", option: "Criar Turma", dropdown:"Criar Turma"},
+  { id: 4, icon:"calendar-month", option: "Criar Aluno", dropdown:"Criar Aluno"},
+  { id: 5, icon:"person-add", option: "Adicionar Aluno", dropdown:"Adicionar Aluno"},
 ];
 
 export default options
