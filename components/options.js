653973be36058662268aafb525dae8cc54bb8053
Jorge <jorgejapao00@gmail.com>
2025-05-03 12:12:10 -0300

    dropdown changed to screen

--- components/options.js
+++ components/options.js
@@ -1,10 +1,10 @@
 // Dados
 const options = [
-  { id: 1, icon:"how-to-reg", option: "Realizar Chamada", dropdown:"realizar_chamada"},
-  { id: 2, icon:"note-add", option: "Adicionar Conteúdo", dropdown:"Adicionar Conteúdo"},
-  { id: 3, icon:"calendar-month", option: "Criar Turma", dropdown:"Criar Turma"},
-  { id: 4, icon:"calendar-month", option: "Criar Aluno", dropdown:"Criar Aluno"},
-  { id: 5, icon:"person-add", option: "Adicionar Aluno", dropdown:"Adicionar Aluno"},
+  { id: 1, icon:"how-to-reg", option: "Realizar Chamada", screen:"realizar_chamada"},
+  { id: 2, icon:"note-add", option: "Adicionar Conteúdo", screen:"Adicionar Conteúdo"},
+  { id: 3, icon:"calendar-month", option: "Criar Turma", screen:"Criar Turma"},
+  { id: 4, icon:"calendar-month", option: "Criar Aluno", screen:"Criar Aluno"},
+  { id: 5, icon:"person-add", option: "Adicionar Aluno", screen:"RegisterScreen"},
 ];
 
 export default options
