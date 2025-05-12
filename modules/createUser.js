fd40e42bb4fce787bd06cbc871478eeed7249abb
Jorge <jorgejapao00@gmail.com>
2025-05-12 19:15:24 -0300

    function to create a new user created

---
+++ modules/createUser.js
@@ -0,0 +1,21 @@
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { doc, setDoc } from 'firebase/firestore';
+import { auth, db } from '../firebase/firebaseConfig';
+
+export const handleRegister = async () => {
+  try {
+    const userCred = await createUserWithEmailAndPassword(auth, email, senha); // Use o CPF como senha só se quiser
+
+    await setDoc(doc(db, 'usuarios', userCred.user.uid), {
+      nome,
+      email,
+      cpf,
+      nascimento,
+      tipo: 'comum'
+    });
+
+    console.log('Usuário criado com sucesso!');
+  } catch (err) {
+    console.error('Erro ao registrar:', err.message);
+  }
+};
