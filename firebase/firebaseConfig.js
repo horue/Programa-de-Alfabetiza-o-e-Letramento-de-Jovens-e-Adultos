931a6942cb0c6485e606bbf25dfee3025a950219
Matheus Ribeiro Binder <148299789+MrBinderUwU@users.noreply.github.com>
2025-05-23 11:18:46 -0300

    deleted firebase config
    firebase config removed for reasons

--- firebase/firebaseConfig.js
+++ firebase/firebaseConfig.js
@@ -9,13 +9,7 @@ import ReactNativeAsyncStorage  from "@react-native-async-storage/async-storage"
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDM4yCUDo6uOz9XOcuWt1zDyYz-5c3FdGk",
-  authDomain: "palja-mobauth.firebaseapp.com",
-  projectId: "palja-mobauth",
-  storageBucket: "palja-mobauth.firebasestorage.app",
-  messagingSenderId: "469240617197",
-  appId: "1:469240617197:web:d5c482a1a59caed9eb14e4",
-  measurementId: "G-4Q4JTZJT1N"
+  
 };
 
 // Initialize Firebase
