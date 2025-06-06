c82335c9727beb7157876fba6b326e7c0cabf989
Jorge <jorgejapao00@gmail.com>
2025-06-06 12:31:56 -0300

    new list of options added

--- components/options.js
+++ components/options.js
@@ -1,5 +1,5 @@
 // Dados
-const options = [
+export const full_options = [
   { id: 1, icon:"how-to-reg", option: "Realizar Chamada", screen:"realizar_chamada"},
   { id: 2, icon:"note-add", option: "Adicionar Conteúdo", screen:"Adicionar Conteúdo"},
   { id: 3, icon:"calendar-month", option: "Criar Turma", screen:"Criar Turma"},
@@ -7,5 +7,11 @@ const options = [
   { id: 5, icon:"person-add", option: "Adicionar Pessoa", screen:"RegisterScreen"},
 ];
 
-export default options
+export const professor_options = [
+  { id: 1, icon:"how-to-reg", option: "Realizar Chamada", screen:"realizar_chamada"},
+  { id: 2, icon:"note-add", option: "Adicionar Conteúdo", screen:"Adicionar Conteúdo"},
+];
+
+
+export default professor_options
 // https://fonts.google.com/icons
