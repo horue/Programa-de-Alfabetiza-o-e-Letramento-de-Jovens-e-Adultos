4cf619a554692ff295c46d2e19a5f4c51d20eeb5
Jorge <jorgejapao00@gmail.com>
2025-06-07 19:54:58 -0300

    Screens changed to function

--- components/methods.js
+++ components/methods.js
@@ -1,5 +1,5 @@
 // Dados
 export const subject_options = [
-  { id: 1, icon:"how-to-reg", option: "Enviar Arquivo", screen:"pickDocument"},
-  { id: 2, icon:"note-add", option: "Tirar uma Foto", screen:"pickDocument"},
+  { id: 1, icon:"folder", option: "Selecionar Arquivo", function:"pickDocument"},
+  { id: 2, icon:"photo-camera", option: "Tirar uma Foto", function:"pickDocument"},
 ];
