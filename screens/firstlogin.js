d11451ee7374faf212b8ea1ec4eefb6cecbbe465
Jorge <jorgejapao00@gmail.com>
2025-06-24 08:23:43 -0300

    View with gap added to input and button

--- screens/firstlogin.js
+++ screens/firstlogin.js
@@ -1,4 +1,4 @@
-import { Text, SafeAreaView, StyleSheet, Image, TextInput, Button, Linking} from 'react-native';
+import { Text, SafeAreaView, StyleSheet, Image, TextInput, Button, Linking, View} from 'react-native';
 import {useState} from 'react';
 
 
@@ -7,7 +7,6 @@ import {useState} from 'react';
 import { Card } from 'react-native-paper';
 
 // Imports from files
-import { styles } from '../styles.js';
 import { CustomButton } from '../components/buttons.js';
 
 // Universal Consts
@@ -26,20 +25,68 @@ export default function FirstLogin({ onLogin }) {
     <SafeAreaView style={styles.container}>
       <Image style={styles.logo} source={require('../assets/logo.png')} />
       <Text style={styles.paragraph}>Detectamos que é a sua primeira vez realizando o login. Por favor, defina uma nova senha antes de continuar.{'\n'}Isso não acontecerá das próximas vezes que você entrar.</Text>
-      <Text style={styles.paragraph}>
-      ----------------------------------------------
-      </Text>
-      <Text style={styles.common_text}>
-        Nova Senha
-      </Text>
-      <TextInput style={styles.input}>
-      </TextInput>
-      <Text style={styles.common_text}>
-        Confirme a senha
-      </Text>
-      <TextInput style={styles.input} secureTextEntry={true}>
-      </TextInput>
-      <CustomButton buttonText='Alterar senha' textAlign='center' textColor='white' buttonColor='#00acbb' onPress={onLogin}></CustomButton>
+      <View style={{gap: 24}}>
+            <Text style={styles.common_text}>
+                Nova Senha
+            </Text>
+            <TextInput style={styles.input}>
+            </TextInput>
+            <Text style={styles.common_text}>
+                Confirme a senha
+            </Text>
+            <TextInput style={styles.input} secureTextEntry={true}>
+            </TextInput>
+            <CustomButton buttonText='Alterar senha' textAlign='center' textColor='white' buttonColor='#00acbb' onPress={onLogin}></CustomButton>
+        </View>
     </SafeAreaView>
   );
 }
+
+
+export const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      backgroundColor: '#ecf0f1',
+      padding: 8,
+    },
+    paragraph: {
+      margin: 24,
+      fontSize: 12,
+      textAlign: 'center',
+    },
+    common_text: {
+      fontSize: 15,
+      color: "#000000",
+      textAlign: "left",
+    },
+    hyperlink:{
+      fontSize: 14,
+      margin: 7,
+      color: "#0000ff",
+      textAlign: "right",
+    },
+    hyperlink_clicked:{
+      fontSize: 14,
+      margin: 7,
+      color: "#000080",
+      textAlign: "right",
+      textDecorationLine: 'underline',
+    },
+    input: {
+      borderWidth: 1,
+      borderRadius: 18,
+      borderColor:"#001a33",
+      height: 38,
+      backgroundColor: "#e0f0ff"
+    },
+      logo: {
+      height: 100,
+      width: 346,
+      resizeMode: "contain",
+      justifyContent: 'center',
+      position: 'center',
+      alignSelf: 'center',
+    },
+  });
+  
