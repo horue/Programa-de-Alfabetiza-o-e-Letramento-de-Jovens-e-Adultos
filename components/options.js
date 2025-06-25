739d466583680e0bccfd1b098b0b1a2bc7b25a07
Jorge <jorgejapao00@gmail.com>
2025-06-25 18:55:01 -0300

    administrator options updated

--- components/options.js
+++ components/options.js
@@ -1,14 +1,14 @@
 // Dados
 export const full_options = [
-  { id: 1, icon:"how-to-reg", option: "Realizar Chamada", screen:"realizar_chamada"},
+  { id: 1, icon:"how-to-reg", option: "Realizar Chamada", screen:"AttendanceScreen"},
   { id: 2, icon:"note-add", option: "Adicionar Conteúdo", screen:"Adicionar Conteúdo"},
-  { id: 3, icon:"calendar-month", option: "Criar Turma", screen:"Criar Turma"},
-  { id: 4, icon:"calendar-month", option: "Criar Aluno", screen:"Criar Aluno"},
-  { id: 5, icon:"person-add", option: "Adicionar Pessoa", screen:"RegisterScreen"},
+  { id: 3, icon:"calendar-month", option: "Criar Turma", screen:"CreateClassScreen"},
+  { id: 5, icon:"person-add", option: "Cadastrar Usuário", screen:"RegisterScreen"},
+  { id: 6, icon:"person-add", option: "Adicionar Aluno à Turma", screen:"AddToClass"},
 ];
 
 export const professor_options = [
-  { id: 1, icon:"how-to-reg", option: "Realizar Chamada", screen:"realizar_chamada"},
+  { id: 1, icon:"how-to-reg", option: "Realizar Chamada", screen:"AttendanceScreen"},
   { id: 2, icon:"note-add", option: "Adicionar Conteúdo", screen:"SubjectScreen"},
 ];
 
