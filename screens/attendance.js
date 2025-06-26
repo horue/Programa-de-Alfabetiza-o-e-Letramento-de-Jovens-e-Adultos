9c225ff1528f7ffb14d6fa9bdb1709dcbbae0c28
Jorge <jorgejapao00@gmail.com>
2025-06-26 03:40:37 -0300

    class is now selectable from database

--- screens/attendance.js
+++ screens/attendance.js
@@ -5,12 +5,15 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
 import { useState, useEffect } from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
+import RNPickerSelect from 'react-native-picker-select';
+
 
 import { full_options } from '../components/options';
 import { alunos } from '../components/test';
 import { CustomButton } from '../components/buttons';
 
 import { getStudentsFromClass } from '../modules/getstudentsclass';
+import { getClass } from '../modules/getClass';
 
 
 
@@ -40,7 +43,7 @@ export const AttendanceComponent = ({ nome, matricula, aberto, onPress }) => {
       <View style={{ flexDirection: 'row', alignItems: 'center' }}>
         <View style={{ gap: 16 }}>
           <Text style={styles.nome}>{nome}</Text>
-          <Text style={styles.matricula}>{matricula}</Text>
+          <Text style={styles.matricula}>Matrícula: {matricula}</Text>
         </View>
         <TouchableOpacity style={{backgroundColor: boxColor,height: 40,width: 40,borderRadius: 15,left: '85%',position: 'absolute', borderColor: boxColor, borderWidth: 2}}
           onPress={checkHandler}>
@@ -51,35 +54,81 @@ export const AttendanceComponent = ({ nome, matricula, aberto, onPress }) => {
   );
 };
 
+const ClassDropdown = ({ onSelect }) => {
+    const [selectedClass, setSelectedClass] = useState('');
+    const [classes, setClasses] = useState([]);
+
+    useEffect(() => {
+        const fetchClasses = async () => {
+            const lista = await getClass();
+            const classesFormatted = lista.map((turma) => ({
+                label: `${turma.codigo} - ${turma.professor}`,
+                value: turma.codigo,
+            }));
+            setClasses(classesFormatted);
+        };
+
+        fetchClasses();
+    }, []);
+
+
+    const handleClassChange = (value) => {
+        setSelectedClass(value);
+        if (onSelect) {
+        onSelect(value);
+        }
+    };
+
+
+  return (
+    <View>
+        <RNPickerSelect
+            onValueChange={handleClassChange}
+            items={classes}
+            placeholder={{ label: 'Turma', value: null }}
+            value={selectedClass}
+        />
+    </View>
+  );
+};
+
+
 const alunosOrdenados = [...alunos].sort((a, b) => {
     return a.nome.localeCompare(b.nome, undefined, { sensitivity: 'base' });
   });
 
-export function AttendanceScreen({onExit}) {
+export function AttendanceScreen({selectedClass}) {
   const [alunos, setAlunos] = useState([]);
+  const [selectedClassCode, setSelectedClassCode] = useState(selectedClass);
+
 
   useEffect(() => {
     const carregarAlunos = async () => {
-      const lista = await getStudentsFromClass('IG202561');
+      const lista = await getStudentsFromClass(selectedClassCode);
       setAlunos(lista);
     };
     carregarAlunos();
-  }, []);
+  },  [selectedClassCode]);
 
   return(
-    <ScrollView style={styles.container} contentContainerStyle={{ alignItems: 'center', gap: 4 }}>
-    {[...alunos].sort((a, b) => (
-        a.nome.localeCompare(b.nome, undefined, { sensitivity: 'base' })
-    )).map((item) => (
-        <AttendanceComponent
-            key={item.matricula}
-            nome={item.nome}
-            matricula={item.matricula}
-        />
-    ))}
-    <CustomButton buttonText={'Confirmar'} buttonColor={'#00acbb'} textColor={'white'}></CustomButton>
-    <Text>{'\n'}</Text>
-    </ScrollView>
+    <>
+      <View>         
+        <ClassDropdown onSelect={setSelectedClassCode}/>
+      </View>
+      <ScrollView style={styles.container} contentContainerStyle={{ alignItems: 'center', gap: 4 }}>
+        {[...alunos].sort((a, b) => (
+            a.nome.localeCompare(b.nome, undefined, { sensitivity: 'base' })
+        )).map((item) => (
+            <AttendanceComponent
+                key={item.matricula}
+                nome={item.nome}
+                matricula={item.matricula}
+            />
+        ))}
+        <CustomButton buttonText={'Confirmar'} buttonColor={'#00acbb'} textColor={'white'}></CustomButton>
+        <Text>{'\n'}</Text>
+      </ScrollView>
+    </>
   )
 };
 
@@ -98,7 +147,7 @@ export const styles = StyleSheet.create({
         borderRadius: 8,
         width: '90%',
     },
-    nome: { fontSize: 16, fontWeight: 'bold'},
+    nome: { fontSize: 17, fontWeight: 'bold'},
     matricula: { fontSize: 16, fontWeight: 'normal' },
 });
 
