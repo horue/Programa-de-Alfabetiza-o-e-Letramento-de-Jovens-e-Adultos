8102ab0ed72657caca17de292f3136d4c0aa0929
Jorge <jorgejapao00@gmail.com>
2025-06-26 20:53:48 -0300

    full options list updated

--- components/options.js
+++ components/options.js
@@ -1,7 +1,7 @@
 // Dados
 export const full_options = [
   { id: 1, icon:"how-to-reg", option: "Realizar Chamada", screen:"AttendanceScreen"},
-  { id: 2, icon:"note-add", option: "Adicionar Conteúdo", screen:"Adicionar Conteúdo"},
+  { id: 2, icon:"note-add", option: "Adicionar Conteúdo", screen:"SubjectScreen"},
   { id: 3, icon:"calendar-month", option: "Criar Turma", screen:"CreateClassScreen"},
   { id: 3.1, icon:"build", option: "Gerenciar turma", screen:"ClassManagementScreen"},
   { id: 5, icon:"person-add", option: "Cadastrar Usuário", screen:"RegisterScreen"},
