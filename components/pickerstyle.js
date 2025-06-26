5236f046a56936506e2f93f978d7345aaf000066
Jorge <jorgejapao00@gmail.com>
2025-06-26 08:53:29 -0300

    pickerStyles added

---
+++ components/pickerstyle.js
@@ -0,0 +1,21 @@
+export const pickerStyles = {
+  inputIOS: {
+    backgroundColor: '#ffffff',  // fundo
+    color: '#333333',            // texto
+    paddingVertical: 12,
+    paddingHorizontal: 16,
+    borderRadius: 12,            // borda arredondada
+    borderWidth: 1,
+    borderColor: '#00acbb',      // cor da borda
+  },
+  inputAndroid: {
+    backgroundColor: 'white',
+    borderColor: '#00acbb',
+    borderRadius: 10,
+  },
+  placeholder: {
+    color: '#999999',
+  }
+};
+
+  
