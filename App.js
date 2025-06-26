d0116e70dc274dc0f90ea525a04c915c4fc8398f
Jorge <jorgejapao00@gmail.com>
2025-06-26 19:35:38 -0300

    App.js moved to a AppProvider

--- App.js
+++ App.js
@@ -1,24 +1,26 @@
 import {useState} from 'react';
 
-
 // Imports from files
 import LoginScreen from './screens/login.js';
 import { MainScreen } from './screens/main.js';
 import FirstLogin from './screens/firstlogin.js';
+import { AppProvider } from './contexts/appcontext.js';
 
 // Universal Consts
 const d = new Date();
 
-
-
 export default function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(''); // Estado aqui
+  const [isLoggedIn, setIsLoggedIn] = useState('3');
 
   return(
-    isLoggedIn == '' ? 
-      <LoginScreen onLogin={() => setIsLoggedIn('firstLogin')}/>: 
-       isLoggedIn == 'firstLogin' ?
-        <FirstLogin onLogin={() => setIsLoggedIn('loggedIn')}/>:
-        <MainScreen onExit={() => setIsLoggedIn('')}/>
+    <AppProvider>
+      {
+        isLoggedIn == '' ? 
+          <LoginScreen onLogin={() => setIsLoggedIn('firstLogin')}/> : 
+        isLoggedIn == 'firstLogin' ?
+          <FirstLogin onLogin={() => setIsLoggedIn('loggedIn')}/> :
+          <MainScreen onExit={() => setIsLoggedIn('')}/>
+      }
+    </AppProvider>
   )
 }
