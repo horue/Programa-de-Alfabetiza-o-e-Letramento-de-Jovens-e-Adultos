69fd9a45e63cbfdc6e5d65e5b9f76778e688e653
Jorge <jorgejapao00@gmail.com>
2025-06-29 16:59:27 -0300

    function test updated

--- tests/function.test.js
+++ tests/function.test.js
@@ -3,12 +3,12 @@ import { OptionsScreen } from '../screens/options';
 
 jest.mock('../contexts/appcontext', () => ({
   useAppContext: () => ({
-    usuario: { cargo: 'aluno' },
+    usuario: { cargo: 'professor' },
   }),
 }));
 
 describe('OptionsScreen', () => {
-  it('não deve renderizar a opção "Cadastrar Usuário" para alunos', () => {
+  it('não deve renderizar a opção "Cadastrar Usuário" para não administradores', () => {
     const { queryByText } = render(<OptionsScreen navigation={{ navigate: jest.fn() }} />);
 
     expect(queryByText('Cadastrar Usuário')).toBeNull();
